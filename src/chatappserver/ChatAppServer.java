24922f2c05f2d3681b41337ed560039b400dec4c
Virtual <omar_m_ashour@yahoo.com>
2018-05-06 00:53:01 +0200

    Displaying the Server IP

--- src/chatappserver/ChatAppServer.java
+++ src/chatappserver/ChatAppServer.java
@@ -5,7 +5,11 @@
  */
 package chatappserver;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.Enumeration;
 
 /**
  *
@@ -38,10 +42,43 @@ public class ChatAppServer {
         }
         return false;
     }
+    
+    public static String getCurrentEnvironmentNetworkIp() {
+        String currentHostIpAddress=null;
+        if (currentHostIpAddress == null) {
+            Enumeration<NetworkInterface> netInterfaces = null;
+            try {
+                netInterfaces = NetworkInterface.getNetworkInterfaces();
+
+                while (netInterfaces.hasMoreElements()) {
+                    NetworkInterface ni = netInterfaces.nextElement();
+                    Enumeration<InetAddress> address = ni.getInetAddresses();
+                    while (address.hasMoreElements()) {
+                        InetAddress addr = address.nextElement();
+                        //                      log.debug("Inetaddress:" + addr.getHostAddress() + " loop? " + addr.isLoopbackAddress() + " local? "
+                        //                            + addr.isSiteLocalAddress());
+                        if (!addr.isLoopbackAddress() && addr.isSiteLocalAddress()
+                                && !(addr.getHostAddress().indexOf(":") > -1)) {
+                            currentHostIpAddress = addr.getHostAddress();
+                        }
+                    }
+                }
+                if (currentHostIpAddress == null) {
+                    currentHostIpAddress = "127.0.0.1";
+                }
+
+            } catch (SocketException e) {
+//                log.error("Somehow we have a socket error acquiring the host IP... Using loopback instead...");
+                currentHostIpAddress = "127.0.0.1";
+            }
+        }
+        return currentHostIpAddress;
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
         listener.start();
-        
+        System.out.println("The Server IP Address ==> "+getCurrentEnvironmentNetworkIp());
     }
     
 }
